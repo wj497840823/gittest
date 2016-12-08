c979d9ef16e40c25037d7bd67842712c66e86b57
wj497840823 <wj497840823@163.com>
2016-12-08 11:16:25 +0800

    git test

---
+++ src/main/java/git/test/GitTest.java
@@ -0,0 +1,13 @@
+package git.test;
+
+/**
+ * Created by wang on 2016/12/8.
+ */
+public class GitTest {
+
+    public static void main(String[] args){
+
+        System.out.println("Hell World !");
+
+    }
+}
